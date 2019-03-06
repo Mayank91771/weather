9ee04e58865e28aaaeacca89ee6bbc7f3186e95e
Mayank91771 <mayankm91771@gmail.com>
2019-03-06 15:47:55 +0530

    this is my first repo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAJqardsc7gC3QdYi2pXb3AzZz7Zqzx-Yg",
+    authDomain: "weather-2b1e6.firebaseapp.com",
+    databaseURL: "https://weather-2b1e6.firebaseio.com",
+    projectId: "weather-2b1e6",
+    storageBucket: "weather-2b1e6.appspot.com",
+    messagingSenderId: "195172808070"
+  }
+};
